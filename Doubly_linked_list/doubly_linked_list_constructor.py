78e25be25ca0b794bf9ed979fde50950effe1ecc
Supun Wickramarachchi <158739212+supunwickramarachchi@users.noreply.github.com>
2025-03-17 13:51:17 +0530

    Update doubly_linked_list_constructor.py
    Added the doubly linked list constructor code to the file

--- Doubly_linked_list/doubly_linked_list_constructor.py
+++ Doubly_linked_list/doubly_linked_list_constructor.py
@@ -1,1 +1,24 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.next = None 
+		self.prev = None
 
+class DoublyLinkedList:
+	def __init__(self, value):
+		new_node = Node(value)
+		self.head = new_node
+		self.tail = new_node
+		self.length = 1
+
+	def print_list(self):
+		temp = self.head
+
+		while temp is not None:
+			print(temp.value)
+			temp = temp.next
+
+
+new_doubly_linked_list = DoublyLinkedList(1)
+
+new_doubly_linked_list.print_list()
